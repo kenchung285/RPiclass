72b4c09571e5687ee7dfc4ad4ab0bfb8bb78ac41
ken <b09901028@ntu.edu.tw>
2023-04-25 19:08:19 +0800

    change library

--- lcd/lcd_adafruit.py
+++ lcd/lcd_adafruit.py
@@ -1,30 +1,33 @@
+import RPi.GPIO as GPIO
+from RPLCD.gpio import CharLCD
 from time import sleep
-from Adafruit_CharLCD import Adafruit_CharLCD
 
-# instantiate lcd and specify pins
-lcd = Adafruit_CharLCD(rs=26, en=19,
-                       d4=13, d5=6, d6=5, d7=11,
-                       cols=16, lines=2)
+GPIO.setwarnings(False)
+
+lcd = CharLCD(pin_rs=26, pin_rw=19, pin_e=13, pins_data=[6, 5, 11, 9], numbering_mode=GPIO.BCM, cols=16, rows=2, dotsize=8)
 lcd.clear()
-# display text on LCD display \n = new line
-lcd.message('Adafruit CharLCD\n  Raspberry Pi')
+lcd.write_string("Adafruit CharLCD\r\n  Raspberry Pi")
 sleep(1)
-# scroll text off display
-for x in range(0, 16):
-    lcd.move_right()
-    sleep(.3)
+
+for i in range(16):
+    lcd.shift_display(1)
+    sleep(0.3)
 sleep(1)
-# scroll text on display
-for x in range(0, 16):
-    lcd.move_left()
-    sleep(.3)
+
+for i in range(16):
+    lcd.shift_display(-1)
+    sleep(0.3)
 sleep(1)
-# scroll text off display
-for x in range(0, 16):
-    lcd.move_left()
-    sleep(.3)
+
+for i in range(16):
+    lcd.shift_display(-1)
+    sleep(0.3)
 sleep(1)
-# scroll text on display
-for x in range(0, 16):
-    lcd.move_right()
-    sleep(.3)
+
+for i in range(16):
+    lcd.shift_display(1)
+    sleep(0.3)
+sleep(1)
+lcd.close(clear=True)
+GPIO.cleanup()
+
